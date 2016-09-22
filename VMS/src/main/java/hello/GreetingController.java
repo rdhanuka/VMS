75a0dbe51edb6b6baadc3214bc8cf288dc5770c6
rdhanuka <rdhanuka@sapient.com>
2016-09-22 21:22:04 +0530

    sample application

---
+++ VMS/src/main/java/hello/GreetingController.java
@@ -0,0 +1,38 @@
+package hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/")
+    public Greeting greeting1( HttpServletRequest request) {
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        try {
+            BufferedReader reader = request.getReader();
+            while ((line = reader.readLine()) != null)
+                jb.append(line);
+        } catch (Exception e) { /*report an error*/ }
+        String s = new String(jb);
+
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, "heelsls"));
+    }
+}
